76580bf381440c504293e1701ad997aed482256d
Ivyliu2019 <liu_chudan@outlook.com>
2017-11-18 15:58:39 -0800

    add month plot functions and plots

--- collaboration_data_processor.py
+++ collaboration_data_processor.py
@@ -1,12 +1,6 @@
 import csv
 import os
 import pickle
-import matplotlib.pyplot as plt
-import numpy as np
-import plotly.plotly as py
-import plotly.tools as tls
-import plotly.graph_objs as go
-from pylab import figure, show
 
 # change the path to yours
 directory = "/Users/IvyLiu/Desktop/Sorted_Data/"
@@ -57,35 +51,41 @@ def main2():
                 List.append(eachLocation)
                 f.close()
     # print(LocationList)
-    print(List)
-    # heatmap1(LocationList, List)
+    # print(List)
+    return List, LocationList
+
+def main3(month):
+    List = []
+    LocationList = []
+    for root,dirs,files in os.walk(directory):
+        for file in files:
+            LocationList.append(file)
+            UserD = {}
+            if file.endswith(".csv"):
+                f=open(directory + file, 'r')
+                filereader = csv.reader(f)
+                listrows = list(filereader)
+                rows = []
+                hours = [str(n) for n in range(0,24)]
+                eachLocation = []
+                for t in hours:
+                    for row in listrows:
+                        if row[1] != "-" and row[5] == str(month) and row[7] == t:
+                            rows.append(row)
+                        elif t== 0 and row[7] == 24:
+                            rows.append(row)
+                    rows = rows[1:]
+                    UserD = findCollaboration(rows, UserD)
+                    print(len(UserD))
+                    eachLocation.append(len(UserD))
+                    UserD = {}
+                    rows = []
+                print(eachLocation)
+                List.append(eachLocation)
+                f.close()
     return List, LocationList
 
 # call this function from main
-def heatmap1(locationlist, intensity):
-    # x = ['6','7','8','9','10','11','12']
-    x = [int(i) for x in range(31) for i in range(6,13)]
-    y = [int(x) for x in range(1, len(locationlist)+1) for i in range(7)]
-    intensity = [item for sublist in intensity for item in sublist]
-    M = np.zeros((max(x) + 1, max(y) + 1))
-    M[x, y] = intensity
-    # print(M)
-
-    fig, ax = plt.subplots()
-    axes = plt.gca()
-    axes.set_ylim([6.5,12.5])
-    axes.set_xlim([0.5,31.5])
-    plt.title('Month vs Location Overlap Frequencies')
-    plt.ylabel('MONTH')
-    plt.xlabel('LOCATION')
-    ax.imshow(M)
-    show()
-
-    # save to file
-    fig.savefig('MonthLocationHeatMap.png', bbox_inches='tight')
-    # and show it on the screen
-    show()
-
 def calculateOverlap(dayStart1, hourStart1, minStart1, dayStart2, hourStart2, minStart2, dayEnd1, hourEnd1, minEnd1, dayEnd2, hourEnd2, minEnd2):
     # Number of minutes since midnight on person 1's start day.
     start1 = hourStart1 * 60 + minStart1
@@ -142,30 +142,9 @@ def findCollaboration(rows, UserD):
 # jRow = ["1419","193b8bff00bdc347489961f3b3b0528ea37a30ceb8ae574bf4d9cc3b73a030b6","cuc","CUC-HON-2-S-RTLS",2016,8,1,7,42,0,8,1,2016,8,int("02")]
 # print (overlap(iRow[6], iRow[7], iRow[8], jRow[6], jRow[7], jRow[8], iRow[11], iRow[13], iRow[14], jRow[11], jRow[13], jRow[14]))
 
-locationlist = ['statsdata CUC-HON-2-S-RTLS .csv', 'statsdata CUC-HON-2-E-BRIDGE .csv', \
-'statsdata CUC-HON-1-E-ELEVATOR .csv', 'statsdata CUC-HON-3-E-ASIAN_STUDIES .csv', \
-'statsdata CUC-MUDD-3-S-KECK2 .csv', 'statsdata CUC-HON-2-S-HALL_BEHIND_ADMIN .csv', \
-'statsdata CUC-HON-3-N-BOOK .csv', 'statsdata CUC-MUDD-1-W-MATERIALS_HANDLING .csv', \
-'statsdata CUC-HON-2-N-WELCOME_DESK .csv', 'statsdata CUC-MUDD-2-NE .csv', \
-'statsdata CUC-HON-3-W-ASIAN_STUDIES .csv', 'statsdata CUC-HON-1-W-CAFE .csv', \
-'statsdata CUC-MUDD-3-N-IRIS .csv', 'statsdata CUC-MUDD-2-W .csv', \
-'statsdata CUC-HON-3-NW-SITTING_AREA .csv', 'statsdata CUC-HON-1-S-CONNECTION .csv', \
-'statsdata CUC-HON-2-W-CONFERENCE_ROOM .csv', 'statsdata CUC-HON-3-S-BOOK .csv', \
-'statsdata CUC-HON-3-W-CENTER .csv', 'statsdata CUC-HON-4-NORTH .csv', \
-'statsdata CUC-HON203-AP105-15 .csv', 'statsdata CUC-MUDD-1-W-ELEVATOR .csv', \
-'statsdata CUC-HON-3-E-BRIDGE .csv', 'statsdata CUC-HON-2-S-STUDY_AREA .csv', \
-'statsdata CUC-HON-1-N-CAFE .csv', 'statsdata CUC-HON-3-E-OUTSIDE_DC .csv', \
-'statsdata CUC-HON-2-S-ADMIN_CONF_ROOM .csv', 'statsdata CUC-HON-4-EAST .csv', \
-'statsdata CUC-MUDD-3-IRIS-N .csv', 'statsdata CUC-HON-1-NE-CAFE .csv', \
-'statsdata CUC-HON-4-SOUTH .csv']
-
-
-intensity = [[0, 0, 77, 194, 131, 161, 207], [0, 0, 206, 825, 760, 1000, 1075], [0, 0, 148, 659, 641, 765, 930], [0, 0, 41, 125, 116, 176, 261], [0, 0, 118, 371, 324, 462, 407], [0, 0, 14, 29, 24, 34, 47], [0, 0, 69, 297, 295, 407, 394], [0, 0, 28, 122, 150, 234, 370], [0, 0, 151, 733, 591, 637, 800], [0, 0, 134, 498, 511, 657, 780], [0, 0, 24, 110, 147, 171, 256], [0, 0, 225, 724, 789, 1032, 816], [0, 1, 40, 166, 223, 252, 267], [0, 0, 106, 503, 458, 535, 584], [0, 0, 73, 245, 262, 385, 346], [0, 0, 114, 285, 269, 314, 309], [0, 0, 58, 255, 254, 329, 315], [0, 1, 151, 516, 519, 705, 695], [0, 0, 75, 255, 294, 417, 514], [0, 0, 38, 132, 158, 180, 300], [0, 0, 30, 106, 107, 132, 152], [0, 0, 32, 116, 128, 200, 286], [0, 0, 156, 567, 531, 700, 741], [0, 0, 105, 839, 755, 881, 663], [0, 0, 127, 444, 452, 606, 636], [0, 0, 122, 453, 472, 554, 594], [0, 0, 58, 175, 208, 238, 317], [0, 0, 77, 348, 374, 453, 543], [0, 1, 39, 81, 71, 79, 62], [0, 0, 306, 960, 948, 1004, 943], [0, 0, 63, 185, 210, 282, 403]]
-
-UserD = heatmap1(locationlist, intensity)
-
-# with open('collaboration_dic.pickle', 'wb') as handle:
-#     pickle.dump(UserD, handle, protocol=pickle.HIGHEST_PROTOCOL)
+UserD = main3(9)
+with open('collaboration_dic.pickle', 'wb') as handle:
+    pickle.dump(UserD, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 #####################################
 # To load pickle
